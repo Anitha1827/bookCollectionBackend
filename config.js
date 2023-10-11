b248f249c71cdf69c3e51a4a754ba23ec27ed286
Anitha <anitha@iweensoft.com>
2023-10-11 22:56:13 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://anithah2711:anitha@cluster0.fts4jt8.mongodb.net/books-collection?retryWrites=true&w=majority";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+// 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority'
